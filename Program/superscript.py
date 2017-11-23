14377adae521fe8c845d12f135511fdb8e631a62
Guilherme Chimello <guilherme.chimello@gmail.com>
2017-11-23 17:01:46 -0200

    Updatedpy
    New stuff, son and things

--- Program/superscript.py
+++ Program/superscript.py
@@ -2,14 +2,44 @@
 ###https://gpiozero.readthedocs.io/en/stable/api_input.html
 
 ##IMPORTS GO HERE
-import gpiozero
-from time import sleep
+import RPi.GPIO as GPIO #GPIO antiga, setup do sistema
+import gpiozero         #GPIO nova, sensores
+import Bluetooth        #Bluetooth
+import subprocess       #processamento paralelo, shell
+import json             #output em json para servidor
+import time             #sleep, time.time
+import threading
 
 ##SETUP
+GPIO.setmode(GPIO.BCM)  #Setup de nome de pinos
+GPIO.setwarnings(False) #desabilita verbosidade de argumentos
+GPIO.setup(pin,GPIO.OUT)#Setup pino como saida
+GPIO.output(pin,0)      #Reseta pino de saida
 waterSensor = gpiozero.InputDevice(pin, pull_up=False)
 waterValve = gpiozero.OutputDevice(pin, active_high=True, initial_value=False)
+shutdown_btn = Button(17, hold_time=2)#Desliga
+shutdown_btn.when_held = shutdown
+mac="macAdress"         #Endereço bluetooth android
+#file.new(server.json)  #Cria documento de servidor
+
 
 ##FUNCTIONS
+
+def startupbtl():
+    subprocess.call(['sudo', 'bluetoothctl'])
+    subprocess.call(['power', 'on'])
+    subprocess.call(['discoverable', 'on'])
+    subprocess.call(['pairable', 'on'])
+    subprocess.call(['scan', 'on'])
+    subprocess.call(['pair <', mac, '>'])
+
+def comms():
+    server_socket=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    port=1
+    server_socket.bind(("",port))
+    server_socket.listen(1)
+    client_socket, address = server_socket.accept()
+    print("Conexão Estabelecida em", address)
 
 def checkWater(x,thresh):
     needsWater = x
@@ -32,32 +62,6 @@ def outputWater(x,y,z):
     else:
         return "The water valve is off"
 
-def recordJSON(x):
-    x=str(x)
-    if str(x)==[::-1]:
-        return "Already on file"
-    else:
-        file.open()
-        file.append(x)
-        file.close()
-        return "Recorded"
-
-def scrUpdateBuffer():
-    file.open()##SERVER FILE
-    sendToBuffer = file.read()##Last Input
-    file.close()##SERVER FILE
-    file.open()##SCREEN BUFFER FILE
-    file.write(sendToBuffer)## CLEAN THE FILE BEFORE WRITING TO IT
-    file.close()##SCREEN BUFFER
-
-def scrRun():
-    ##Run GUI SCRIPT
-    file.open()
-    uiStuff = file.read()##STUFF FROM THE BUFFER FILE
-    exec(open("./GUI.py").read(), uiStuff)
-    file.close()
-    exec(close("./GUI.py"))
-
 def endOfCycle(x):
     sleep(x)
     return 0
@@ -65,21 +69,83 @@ def endOfCycle(x):
 def shutdown():
     check_call(['sudo', 'poweroff'])
 
-###CODE FOR SHUTDOWN BUTTON:
-##shutdown_btn = Button(17, hold_time=2)
-##shutdown_btn.when_held = shutdown
-
-
-##SERVER SCRIPT
-
-##JSON OUTPUT
+##JSON Server
+def server():
+    outputDict = {
+    'logFrequency':60,
+    'numberOfLogs':1,
+    'logs':[{
+        'date':'12/11/2017',
+        'time':'06:11:00',
+        'numberOfSensors':4,
+        'sensors':
+            [{"name":"Temperature",
+            "data":'22.3',
+            "unit":"°C"},
+            {"name":"Moisture",
+            "data":35,
+            "unit":"%"},
+            {"name":"Luminosity",
+            "data":11000,
+            "unit":"Lux"},
+            {"name":"Soil Moisture",
+            "data":"Low",
+            "unit":""}]
+            }]
+    }
+    ##JSON OUTPUT
+    ##file.open(server.json)
+    ##file.append(json.dumps(outputDict))
+    ##file.close(server.json)
 
 ##ACTUATING
 
 ##FEEDBACK LOOP
-if __name__='__main__'
-    scrRun()
+if __name__='__main__':
+    startupbtl()
+##Setup Threading
+    try:
+        thread.start_new_thread(comms,("Thread-1", 2,))
+        thread.start_new_thread(server,("Thread-2", 4,))
+    except:
+        print "Error: unable to start thread"
     outputWater(checkWater(pin), time.time, pin)
-    recordJSON(checkWater(pin))
+    data=client_socket.recv(1024)
+    fix(data)
+    server()
     endOfCycle(10)
-    ##THIS
+    ##End BluetoothSocket
+    ##client_socket.close()
+    ##server_socket.close()
+
+
+## exitFlag = 0
+##
+## class myThread (threading.Thread):
+##    def __init__(self, threadID, name, counter):
+##       threading.Thread.__init__(self)
+##       self.threadID = threadID
+##       self.name = name
+##       self.counter = counter
+##    def run(self):
+##       print "Starting " + self.name
+##       print_time(self.name, 5, self.counter)
+##       print "Exiting " + self.name
+##
+## def print_time(threadName, counter, delay):
+##    while counter:
+##       if exitFlag:
+##          threadName.exit()
+##       time.sleep(delay)
+##       print "%s: %s" % (threadName, time.ctime(time.time()))
+##       counter -= 1
+##
+## # Create new threads
+## thread1 = myThread(1, "Thread-1", 1)
+## thread2 = myThread(2, "Thread-2", 2)
+##
+## # Start new Threads
+## thread1.start()
+## thread2.start()
+##
+## print "Exiting Main Thread"
