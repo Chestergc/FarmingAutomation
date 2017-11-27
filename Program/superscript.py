a6419f85008624c773f9ef87af947560fde1d5cb
Guilherme Chimello <guilherme.chimello@gmail.com>
2017-11-27 13:50:56 -0200

    Again

--- Program/superscript.py
+++ Program/superscript.py
@@ -22,6 +22,19 @@ shutdown_btn.when_held = shutdown
 mac="macAdress"         #Endereço bluetooth android
 #file.new(server.json)  #Cria documento de servidor
 
+#CLASSES
+
+class bluetooth (threading.Thread):
+    def __init__(self, threadID, name, counter):
+       threading.Thread.__init__(self)
+       self.threadID = threadID
+       self.name = name
+       self.counter = counter
+    def run(self):
+       print ("Starting " + self.name)
+       startupbtl()
+       comms()
+       print ("Exiting " + self.name)
 
 ##FUNCTIONS
 
@@ -98,17 +111,11 @@ def server():
     ##file.append(json.dumps(outputDict))
     ##file.close(server.json)
 
-##ACTUATING
-
 ##FEEDBACK LOOP
 if __name__='__main__':
-    startupbtl()
-##Setup Threading
-    try:
-        thread.start_new_thread(comms,("Thread-1", 2,))
-        thread.start_new_thread(server,("Thread-2", 4,))
-    except:
-        print "Error: unable to start thread"
+    bltThread = bluetooth(1, "Comms-1", 1)
+    bltThread.start()
+
     outputWater(checkWater(pin), time.time, pin)
     data=client_socket.recv(1024)
     fix(data)
@@ -117,35 +124,3 @@ if __name__='__main__':
     ##End BluetoothSocket
     ##client_socket.close()
     ##server_socket.close()
-
-
-## exitFlag = 0
-##
-## class myThread (threading.Thread):
-##    def __init__(self, threadID, name, counter):
-##       threading.Thread.__init__(self)
-##       self.threadID = threadID
-##       self.name = name
-##       self.counter = counter
-##    def run(self):
-##       print "Starting " + self.name
-##       print_time(self.name, 5, self.counter)
-##       print "Exiting " + self.name
-##
-## def print_time(threadName, counter, delay):
-##    while counter:
-##       if exitFlag:
-##          threadName.exit()
-##       time.sleep(delay)
-##       print "%s: %s" % (threadName, time.ctime(time.time()))
-##       counter -= 1
-##
-## # Create new threads
-## thread1 = myThread(1, "Thread-1", 1)
-## thread2 = myThread(2, "Thread-2", 2)
-##
-## # Start new Threads
-## thread1.start()
-## thread2.start()
-##
-## print "Exiting Main Thread"
